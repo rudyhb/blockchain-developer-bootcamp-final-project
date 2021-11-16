924e98de036f637e2754ff0536d2e17ad5d27cd0
Rudy <rudyhasbun@gmail.com>
2021-11-16 16:42:32 -0500

    .

--- ui/src/connectors.ts
+++ ui/src/connectors.ts
@@ -1,7 +1,28 @@
 // from @web3-react
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { supportedChainIds } from "./constants/web3Constants";
+import { NetworkConnector } from "@web3-react/network-connector";
+import env from "react-dotenv";
+
+console.log('env:', env)
 
 export const injected = new InjectedConnector({
   supportedChainIds: supportedChainIds.map(c => c.chainId)
 });
+
+let network: NetworkConnector | null = null;
+if (env.RPC_URL && env.RPC_CHAIN_ID) {
+  const chainId = parseInt(env.RPC_CHAIN_ID);
+  if (chainId) {
+    const RPC_URLS: { [chainId: number]: string } = {
+      [chainId]: env.RPC_URL as string
+    };
+
+    network = new NetworkConnector({
+      urls: { [chainId]: RPC_URLS[chainId] },
+      defaultChainId: chainId
+    });
+  }
+}
+
+export { network };
