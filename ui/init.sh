a212cec8ab1a7b99f7df9455a40ca812127c9f3d
Rudy <rudyhasbun@gmail.com>
2021-11-29 14:49:50 -0600

    .

--- ui/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -e ./src/web3Info/develop-details.json ]
-then
-    echo "develop-details.json already exists"
-else
-    echo "creating an empty develop-details.json"
-    echo "{}" > ./src/web3Info/develop-details.json
-fi
